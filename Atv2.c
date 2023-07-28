99fa1828e78d398d75b6ac2780205c2be5f3098c
carla-coder <carla-coder@yahoo.com.br>
2023-07-28 10:39:58 -0300

    aula1

---
+++ Atv2.c
@@ -0,0 +1,14 @@
+#<inclde <stdio.h>
+int main(){
+
+	int velocidade;
+	int distancia;
+	printf("Digite a velocidade em Km/h:")
+	scanf("%d",&distancia);
+	printf("Digite a distancia em km:");
+	scanf("%d",&distancia);
+	int tempo = distancia / velocidade;
+	printf("O tempo em horas é: %d",tempo);
+	return 0;
+}
+	
